5965e4a7c9022571e4b96d1ac14e1ca5699959c6
Omer Sabah <omerrj99@gmail.com>
2021-04-23 22:40:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#000000";
 dodger.style.backgroundColor = "#FF69B4";
 
+<<<<<<< HEAD
 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
@@ -25,4 +26,13 @@ document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   }else if(e.key==='ArrowRight')moveDodgerRight();
+=======
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+ 
+    dodger.style.left = `${left - 1}px`;
+  }
+>>>>>>> c54b2ebb73a5d23e70a81b6ca1e5cdf40e6bbf74
 });
